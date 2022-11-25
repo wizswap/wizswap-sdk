d5f94378f3a1ad41df4016609380ebb6a09c8584
levi0x <levi0x0@proton.me>
2022-11-25 15:54:09 +0200

    rebuild

--- dist/entities/token.d.ts
+++ dist/entities/token.d.ts
@@ -25,6 +25,6 @@ export declare class Token extends Currency {
  */
 export declare function currencyEquals(currencyA: Currency, currencyB: Currency): boolean;
 export declare const WETH: {
-    18159: Token;
+    54321: Token;
     97: Token;
 };
